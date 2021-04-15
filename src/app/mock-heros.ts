e810b5a6b8fb6300a525bafbe1dee82aef2d3cac
N00b00dY <P.Hackel@gmx.de>
2021-04-15 21:26:19 +0200

    works

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,16 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero [] = [
+    { id: 11, name: 'Dr Ich'},
+    { id: 12, name: 'Dr Du'},
+    { id: 13, name: 'Dr Er'},
+    { id: 14, name: 'Dr Sie'},
+    { id: 15, name: 'Dr Es'},
+    { id: 16, name: 'Dr Ihr'},
+    { id: 17, name: 'Dr Sies'},
+    { id: 18, name: 'Dr Wir'},
+    { id: 19, name: 'Dr Nein'},
+    { id: 20, name: 'Dr Ja'},
+    { id: 21, name: 'Dr Yes'}
+
+]
